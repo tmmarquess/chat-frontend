5ee8f13cba8caa19b831b3364c8568bd44f7a881
Debora Souza Barros <dsb.snf21@uea.edu.br>
2023-12-20 02:24:46 -0300

    creating and styling the static chat page

--- src/pages/Chat/index.jsx
+++ src/pages/Chat/index.jsx
@@ -1,15 +1,42 @@
 import React from "react"
-import { PeopleList, SearchPeople } from "./style"
+import { SearchPeople, 
+        Sidebar, 
+        Container, 
+        ChatScreen, 
+        ContentCont, 
+        Decoration, 
+        Title, 
+        ButtonGroup, 
+        ContainerBar, 
+        ContactContainer ,
+        ChatTitle
+    } from "./style"
 
 export function Chat(){
+   
     return(
         <>
-        <SearchPeople>
-            Search User
-        </SearchPeople>
-        <PeopleList>
-            aaa
-        </PeopleList>
+        <Container>
+            <SearchPeople>
+                <Decoration>
+                    Search User
+                </Decoration>
+            </SearchPeople>
+            <ContentCont>
+                <Sidebar>
+                    <ContainerBar>
+                        <Title>My chats</Title>
+                        <ButtonGroup>New Group Chat +</ButtonGroup> 
+                    </ContainerBar>  
+                    <ContactContainer>
+
+                    </ContactContainer>
+                </Sidebar>
+                <ChatScreen>
+                    <ChatTitle>Click on a user to start chatting</ChatTitle>
+                </ChatScreen>
+            </ContentCont>
+        </Container>
         </>
     )
 }
--- src/pages/Chat/style.jsx
+++ src/pages/Chat/style.jsx
@@ -1,14 +1,77 @@
 import styled from 'styled-components'
 
-export const SearchPeople = styled.div`
-    border: 1px solid red;
-    width: 100wh;
-    
+export const Container = styled.div`
+  background: -webkit-linear-gradient(left, #0ebeff, #ff42b3);
+  height: 100vh;
 `
 
-export const PeopleList = styled.div`
-    border: 1px solid red;
-    width: 300px;
-    height: 100vh;
+export const SearchPeople = styled.div`
+    background: #f0f0f0;
+    width: 100wh;
+    height: 60px;
+    margin-bottom: 20px;
+    display: flex;
+`
+export const Decoration = styled.div`
+  background: #fff;
+  margin-left: 5px;
+  margin-top: 5px; 
+  margin-bottom: 5px;
+  width: 1320px;
+  display: flex;
+`
 
+export const ContentCont = styled.div`
+  display: flex;
+  height: 510px;
+`
+
+export const Sidebar = styled.div`
+  border-radius: 2%;
+  background: #fff;
+  margin-left: 15px;
+  width: 470px;
+  margin-right: 15px;
+`
+export const ContainerBar = styled.div`
+  display: flex;
+  margin-left: 25px;
+  margin-right: 25px;
+  justify-content: space-between;
+`
+
+export const Title = styled.p`
+  margin-top: 17px;
+  font-size: 30px;
+`
+
+export const ButtonGroup = styled.button`
+  margin-top: 17px;
+  border: none;
+  background: #f0f0f0;
+  border-radius: 5px;
+  width: 180px;
+  height: 40px;
+`
+
+export const ContactContainer = styled.div`
+  background: #f5f5f5;
+  height: 400px;
+  margin-top: -5px;
+  margin-left: 15px;
+  margin-right: 15px;
+  border-radius: 2%;
+`
+
+export const ChatScreen = styled.div`
+  border-radius: 2%;
+  background: #fff;
+  width: 810px;
+  text-align: center;
+`
+
+export const ChatTitle = styled.p`
+  font-size: 30px;
+  display: inline-block;
+  margin-top: 220px;
 `
